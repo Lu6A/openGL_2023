098c488a2fc646977a02b6a9e1c0ed34d7648b52
lu6 <lucieaugier28@gmail.com>
2023-05-23 11:05:51 +0200

    🏗️ nouveaux essais - segmentation fault

---
+++ src/boids/field.hpp
@@ -0,0 +1,18 @@
+#pragma once
+
+#include "boids.hpp"
+
+class Field {
+private:
+    size_t            N;     // nombre de boids dans le fiels
+    std::vector<Boid> boids; // vecteur qui prend les boids
+    float             area;  // area autour d'un boid (ref pour appliquer les forces)
+
+public:
+    Field(size_t N, const p6::Context& ctx); // constructeur paramétrique
+    ~Field() = default;
+
+    void               fieldDraw(p6::Context& ctx);
+    void               applyRules(strengths strengths);
+    std::vector<Boid>& getBoids() { return boids; }
+};
