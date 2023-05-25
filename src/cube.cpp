50b94f3976681e3bad65c0fa2cfd0add39395aa5
Adriiix <adrrr2000@gmail.com>
2023-05-25 18:17:37 +0200

    🚧essais affichage cube non réussis

--- src/cube.cpp
+++ src/cube.cpp
@@ -51,7 +51,7 @@ Cube::Cube(float size)
     glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, ibo);
 
     glBindBuffer(GL_ARRAY_BUFFER, vbo);
-    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), (void*)0);
+    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), (void*)nullptr);
     glEnableVertexAttribArray(0);
 
     glBindBuffer(GL_ARRAY_BUFFER, 0);
--- src/main.cpp
+++ src/main.cpp
@@ -3,6 +3,7 @@
 #include "boids/boids.hpp"
 #include "boids/field.hpp"
 #include "boids/strengths.hpp"
+#include "cube.hpp"
 #include "glimac/common.hpp"
 #include "glimac/freeflyCamera.hpp"
 #include "glimac/sphere_vertices.hpp"
@@ -40,19 +41,16 @@ int main()
     );
     shader.use();
 
-    // // récupération des locations de variables uniformes
+    // récupération des locations de variables uniformes
     GLuint U_MVP_MATRIX_LOCATION    = glGetUniformLocation(shader.id(), "uMVPMatrix");
     GLuint U_MV_MATRIX_LOCATION     = glGetUniformLocation(shader.id(), "uMVMatrix");
     GLuint U_NORMAL_MATRIX_LOCATION = glGetUniformLocation(shader.id(), "uNormalMatrix");
-    // GLint  U_TEXTURE_1              = glGetUniformLocation(shader.id(), "uTexture1");
-    // GLint  U_TEXTURE_2              = glGetUniformLocation(shader.id(), "uTexture2");
 
     glEnable(GL_DEPTH_TEST);
 
-    // // matrices ?
-    // glm::mat4 ProjMatrix   = glm::perspective(glm::radians(70.0f), 800.0f / 600.0f, 0.1f, 100.0f); // param perspective(float fovy, float aspect, float znear, float far)
-    // glm::mat4 MVMatrix     = glm::translate(glm::mat4(1), glm::vec3(0, 0, -5));
-    // glm::mat4 NormalMatrix = glm::transpose(glm::inverse(MVMatrix));
+    Cube cube(0.5);
+    shader.use();
+    cube.draw();
 
     Model mouche = Model();
 
@@ -84,11 +82,9 @@ int main()
     // activation des attributs de vertex
     static constexpr GLuint VERTEX_ATTR_POSITION = 0;
     static constexpr GLuint VERTEX_ATTR_NORMAL   = 1;
-    // static constexpr GLuint VERTEX_ATTR_TEXCOORDS = 2;
 
     glEnableVertexAttribArray(VERTEX_ATTR_POSITION);
     glEnableVertexAttribArray(VERTEX_ATTR_NORMAL);
-    // glEnableVertexAttribArray(VERTEX_ATTR_TEXCOORDS);
 
     glBindBuffer(GL_ARRAY_BUFFER, vbo);
 
@@ -96,26 +92,14 @@ int main()
 
     glVertexAttribPointer(VERTEX_ATTR_NORMAL, 2, GL_FLOAT, GL_FALSE, sizeof(glimac::ShapeVertex), (const GLvoid*)offsetof(glimac::ShapeVertex, normal));
 
-    // // activation des attributs de vertex
-    // static constexpr GLuint VERTEX_ATTR_POSITION = 0;
-    // static constexpr GLuint VERTEX_NORMAL        = 1;
-    // static constexpr GLuint TEXT_COORD           = 2;
-
     glBindBuffer(GL_ARRAY_BUFFER, 0);
     glBindVertexArray(0);
-
-    // // spécification des attributs de vertex
-    // glBindBuffer(GL_ARRAY_BUFFER, vbo);
-    // glVertexAttribPointer(VERTEX_ATTR_POSITION, 3, GL_FLOAT, GL_FALSE, sizeof(glimac::ShapeVertex), (const GLvoid*)offsetof(glimac::ShapeVertex, position));
-    // glVertexAttribPointer(VERTEX_NORMAL, 3, GL_FLOAT, GL_FALSE, sizeof(glimac::ShapeVertex), (const GLvoid*)offsetof(glimac::ShapeVertex, normal));
-    // glVertexAttribPointer(TEXT_COORD, 2, GL_FLOAT, GL_FALSE, sizeof(glimac::ShapeVertex), (const GLvoid*)offsetof(glimac::ShapeVertex, texCoords));
-    // glBindBuffer(GL_ARRAY_BUFFER, 0);
-    // glBindVertexArray(0);
 
     Field field(50, ctx);
 
     ctx.update = [&]() {
         shader.use();
+        cube.draw();
         std::vector<glm::vec3> positions = field.fieldDraw(ctx);
         field.applyRules(strengths);
 
@@ -129,21 +113,18 @@ int main()
         glm::mat4 MVMatrix     = glm::translate(glm::mat4(1), glm::vec3(0, 0, -5));
         glm::mat4 NormalMatrix = glm::transpose(glm::inverse(MVMatrix));
 
-        for (size_t i = 0; i < field.getBoids().size(); i++)
-        {
-            MVMatrix = glm::translate(glm::mat4{1.f}, {0.f, 0.f, 0.f}); // Translation
-            MVMatrix = glm::translate(MVMatrix, positions[i]);          // Translation * Rotation * Translation
-            MVMatrix = glm::scale(MVMatrix, glm::vec3{0.02f});
-            MVMatrix = viewMatrix * MVMatrix;
-            // glm::mat4 MVMatrixBoids = glm::translate(glm::mat4{1.f}, {1.f, 1.f, -1.f}); // Translation
-            // MVMatrixBoids           = glm::translate(MVMatrixBoids, positions[i]);      // Translation * Rotation * Translation
-            // MVMatrixBoids           = glm::scale(MVMatrixBoids, glm::vec3{1., 1., 1.}); // Translation * Rotation * Translation * Scale
-            // MVMatrixBoids           = MVMatrix * MVMatrixBoids;
-            glUniformMatrix4fv(U_MVP_MATRIX_LOCATION, 1, GL_FALSE, glm::value_ptr(ProjMatrix * MVMatrix));
-            glUniformMatrix4fv(U_MV_MATRIX_LOCATION, 1, GL_FALSE, glm::value_ptr(MVMatrix /*Boids*/));
-            glUniformMatrix4fv(U_NORMAL_MATRIX_LOCATION, 1, GL_FALSE, glm::value_ptr(NormalMatrix));
-            glDrawArrays(GL_TRIANGLES, 0, m_vertices.size());
-        };
+        // for (size_t i = 0; i < field.getBoids().size(); i++)
+        // {
+        //     MVMatrix = glm::translate(glm::mat4{1.f}, {0.f, 0.f, 0.f}); // Translation
+        //     MVMatrix = glm::translate(MVMatrix, positions[i]);          // Translation * Rotation * Translation
+        //     MVMatrix = glm::scale(MVMatrix, glm::vec3{0.02f});
+        //     MVMatrix = viewMatrix * MVMatrix;
+
+        //     glUniformMatrix4fv(U_MVP_MATRIX_LOCATION, 1, GL_FALSE, glm::value_ptr(ProjMatrix * MVMatrix));
+        //     glUniformMatrix4fv(U_MV_MATRIX_LOCATION, 1, GL_FALSE, glm::value_ptr(MVMatrix /*Boids*/));
+        //     glUniformMatrix4fv(U_NORMAL_MATRIX_LOCATION, 1, GL_FALSE, glm::value_ptr(NormalMatrix));
+        //     glDrawArrays(GL_TRIANGLES, 0, m_vertices.size());
+        // };
 
         // debinder le vbo
         glBindVertexArray(0);
