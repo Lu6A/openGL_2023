d80d4ebe648ac07a054464318611b26f55ca7173
Adriiix <adrrr2000@gmail.com>
2023-06-01 23:26:17 +0200

    🚧 détails

--- src/boids/boids.cpp
+++ src/boids/boids.cpp
@@ -39,53 +39,36 @@ glm::vec3 Boid::render(p6::Context& ctx)
     //  donne les nouveaux coordonnées en fonction des vélocités
     m_position += m_velocity;
 
-    // // // pour la continuité sur l'axe x;
-    // if (std::abs(m_position.x) > ctx.aspect_ratio())
-    // {
-    //     m_velocity.x *= -1;
-    // }
-
-    // // pour la continuité sur l'axe y;
-    // if (std::abs(m_position.y) > 1)
-    // {
-    //     m_velocity.y *= -1;
-    // }
-
-    // if (m_position.z > -1 || m_position.z < -5)
-    // {
-    //     m_velocity.z *= -1;
-    // }
-
-    if (m_position.x > 2.f)
+    if (m_position.x > 3.5f)
     {
-        m_position.x = 2.f;
+        m_position.x = 3.5f;
         m_velocity.x *= -1;
     }
-    else if (m_position.x < -2.f)
+    else if (m_position.x < -3.5f)
     {
-        m_position.x = -2.f;
+        m_position.x = -3.5f;
         m_velocity.x *= -1;
     }
 
-    if (m_position.y > 2.f)
+    if (m_position.y > 3.8f)
     {
-        m_position.y = 2.f;
+        m_position.y = 3.8f;
         m_velocity.y *= -1;
     }
-    else if (m_position.y < -2.f)
+    else if (m_position.y < -3.8f)
     {
-        m_position.y = -2.f;
+        m_position.y = -3.8f;
         m_velocity.y *= -1;
     }
 
-    if (m_position.z > 2.f)
+    if (m_position.z > 2.8f)
     {
-        m_position.z = 2.f;
+        m_position.z = 2.8f;
         m_velocity.z *= -1;
     }
-    else if (m_position.z < -2.f)
+    else if (m_position.z < -2.8f)
     {
-        m_position.z = -2.f;
+        m_position.z = -2.8f;
         m_velocity.z *= -1;
     }
 
